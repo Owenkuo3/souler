2bc7d2b910cf68ffd95d27b37752e0a761930ef8
Owen Kuo <a74109876@gmail.com>
2025-04-23 22:10:00 +0800

    Initial commit

--- astro_project/accounts/views.py
+++ astro_project/accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .forms import RegistrationForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        if form.is_valid():
+            #
+            cleaned_data = form.cleaned_data
+            return render(request, 'accounts/result.html', {'data': cleaned_data})
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'accounts/register.html', {'form': form})
