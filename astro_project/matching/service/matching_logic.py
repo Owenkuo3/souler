92441e1c903e57f528398531296b6d6cbccfbbfc
Owen kuo <owen.kuo@TWLW83410-M.local>
2025-05-29 16:05:41 +0800

    push

--- astro_project/matching/service/matching_logic.py
+++ astro_project/matching/service/matching_logic.py
@@ -6,7 +6,7 @@ from matching.service.aspect_matching import calculate_match_score
 def get_matching_candidates(user, top_n=10):
     my_profile = user.profile
     preferred_gender = my_profile.match_gender
-
+    
     # 初步過濾
     candidates = UserProfile.objects.exclude(user=user)
     if preferred_gender != 'A':
